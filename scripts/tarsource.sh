fe497afca298fc1769fcdfb8bbd58f817db94498
Vinicius Galvao Mucuge <vgm@Viniciuss-MacBook-Pro.local>
2017-11-09 02:10:27 +0100

    Fix script tarsource.sh

--- scripts/tarsource.sh
+++ scripts/tarsource.sh
@@ -7,8 +7,8 @@ fi
 
 mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
 echo '%_topdir %(echo $HOME)/rpmbuild' > ~/.rpmmacros
-mv nr-integrations foldersize-integration-$VERSION
-tar czvf foldersize-integration-$VERSION.tar.gz foldersize-integration-$VERSION
-mv foldersize-integration-$VERSION.tar.gz ~/rpmbuild/SOURCES
+mv nr-integrations foldersize_integration-$VERSION
+tar czvf foldersize_integration-$VERSION.tar.gz foldersize_integration-$VERSION
+mv foldersize_integration-$VERSION.tar.gz ~/rpmbuild/SOURCES/
 cp specs/foldersize_integration.spec ~/rpmbuild/SPECS/foldersize_integration.spec
 sed -i s/VERSION/$VERSION/g ~/rpmbuild/SPECS/foldersize_integration.spec
