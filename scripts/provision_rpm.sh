35f85380b54233edf613cc4a08d7f0be0d68f3ab
Vinicius Mucuge <viniciusmucuge@gmail.com>
2017-11-13 01:36:53 +0100

    Fix provision script

--- scripts/provision_rpm.sh
+++ scripts/provision_rpm.sh
@@ -8,9 +8,11 @@ sudo yum install epel-release
 echo "== Installing NR Infrastructure"
 sudo curl -o /etc/yum.repos.d/newrelic-infra.repo https://download.newrelic.com/infrastructure_agent/linux/yum/el/7/x86_64/newrelic-infra.repo
 sudo yum -q makecache -y --disablerepo='*' --enablerepo='newrelic-infra'
+sudo yum install -y newrelic-infra
 sudo curl -o /etc/yum.repos.d/integration-infra.repo http://ci-poc.vmucuge-test.tk:81/repo/integration.repo
+sudo yum -q makecache -y --enablerepo='*'
 echo "license_key: $NR_LICENSE_KEY" | tee -a /etc/newrelic-infra.yml
-sudo yum install -i newrelic-infra ansible foldersize_integration
+sudo yum -y install ansible foldersize_integration
 sudo yum -y install https://releases.hashicorp.com/vagrant/2.0.1/vagrant_2.0.1_x86_64.rpm
 
 extip=$(curl -s http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip -H "Metadata-Flavor: Google")
