9f1b130477fab44e08fa6b2aebf4063d77c347d3
Vinicius Mucuge <viniciusmucuge@gmail.com>
2017-11-15 12:03:12 +0100

    Add rpmmacros debug

--- scripts/buildrpm.sh
+++ scripts/buildrpm.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-
+RPM="${1:-foldersize_integration}"
+RPMBUILD="$HOME/rpmbuild"
+if [ -z $1 ] ; then
+  echo "Set the name of the Package"
+  echo "i.e. $0 foldersize_integration"
+fi
 #cd ~/rpmbuild/SOURCES && spectool -g ../SPECS/foldersize_integration.spec
-cd ~/rpmbuild
-rpmdbuild -bb SPECS/foldersize_integration.spec
+if [ ! -d $RPMBUILD ] ; then
+  ./rpmbuilder.sh
+  echo "RPM Build Dirs created"
+fi
+
+cd $RPMBUILD
+rpmdbuild -bb SPECS/$RPM.spec
