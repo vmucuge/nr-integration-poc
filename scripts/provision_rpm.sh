f257e36b2cf3e49b319e9daf8f1ed0a91c9e35b5
Vinicius Mucuge <viniciusmucuge@gmail.com>
2017-11-15 12:04:17 +0100

    Clean script to provision Centos

--- scripts/provision_rpm.sh
+++ scripts/provision_rpm.sh
@@ -4,17 +4,24 @@ ver=$(cat /etc/centos-release)
 echo "== BEGIN: vagrant provisioning on '${uname}'"
 echo "== OS VERSION: ${ver}"
 echo "== Installing Epel Release Repo"
-sudo yum install epel-release
+sudo yum install -y epel-release
 echo "== Installing NR Infrastructure"
 sudo curl -o /etc/yum.repos.d/newrelic-infra.repo https://download.newrelic.com/infrastructure_agent/linux/yum/el/7/x86_64/newrelic-infra.repo
 sudo yum -q makecache -y --disablerepo='*' --enablerepo='newrelic-infra'
 sudo yum install -y newrelic-infra
+sudo systemctl enable newrelic-infra
+echo "license_key: $NR_LICENSE_KEY" | tee -a /etc/newrelic-infra.yml
+
+# integration-infra
 sudo curl -o /etc/yum.repos.d/integration-infra.repo http://ci-poc.vmucuge-test.tk:81/repo/integration.repo
 sudo yum -q makecache -y --enablerepo='*'
-echo "license_key: $NR_LICENSE_KEY" | tee -a /etc/newrelic-infra.yml
-sudo yum -y install ansible foldersize_integration
-sudo yum -y install https://releases.hashicorp.com/vagrant/2.0.1/vagrant_2.0.1_x86_64.rpm
+sudo yum -y install foldersize_integration
 
+# Extras provision and automation
+sudo yum install -y ansible
+sudo yum install -y https://releases.hashicorp.com/vagrant/2.0.1/vagrant_2.0.1_x86_64.rpm
+
+# GCP Specifics
 extip=$(curl -s http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip -H "Metadata-Flavor: Google")
 echo "== EXTERNAL IP: ${extip}"
 echo "== APPENDING /etc/motd"
