9eb41ab260c05d0825f85d493d24ea10008604da
Vinicius Mucuge <viniciusmucuge@gmail.com>
2017-11-15 12:06:11 +0100

    Clean rpmbuilder script

--- scripts/rpmbuilder.sh
+++ scripts/rpmbuilder.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+RPMBUILD="$HOME/rpmbuild"
 echo "Install RPM Build Library"
 sudo yum install rpm-build
-sudo yum install chroot redhat-rpm-config m4 gcc-c++ autoconf automake ncurses-devel wget -y 
+sudo yum install chroot redhat-rpm-config m4 gcc-c++ autoconf automake ncurses-devel wget -y
 
-mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
-echo '%_topdir %(echo $HOME)/rpmbuild' > ~/.rpmmacros
+mkdir -p "$RPMBUILD/{BUILD,RPMS,SOURCES,SPECS,SRPMS}"
+echo '%_topdir %(echo $HOME)/rpmbuild' > "$HOME/.rpmmacros"
+echo '%debug_package %{nil}' >> "$HOME/.rpmmacros"
